d2d3d50e2ba0039841f6a81ad1b43ea43f8d5aa2
Nghia <thealmighty10110@gmail.com>
2023-07-06 03:39:46 +0000

    fix bugs. final answer

--- inversions_count.py
+++ inversions_count.py
@@ -1,44 +1,32 @@
 def mergeAndCount(a):
-    if len(a) == 1:
-        print('NO INVERSION')
-        return 0
+    n = len(a)
+    if n == 1:
+        return a, 0
     else:
-        count = 0
-        n = len(a)
-        # print(n)
-        if n == 2:
-            if a[0] > a[1]:
-                temp = a[0]
-                a[0] = a[1]
-                a[1] = temp
-                return 1
+        b, x= mergeAndCount(a[:n//2])
+        c, y= mergeAndCount(a[n//2:])
+        d, z=a,0
+        i,j=0,0
+        for k in range(0,n):
+            # print(i,j)
+            if (i < n//2) & (j < n//2):
+                if (b[i] < c[j]):
+                    d[k] = b[i] 
+                    i+=1
+                elif (b[i] > c[j]):
+                    d[k] = c[j]
+                    j+=1
+                    z+= len(b[i:])
             else:
-                return 0
-        else:
-            b = a[:(n//2)]
-            # print(b)
-            c = a[(n//2):]
-            # print(c)
-            count+=mergeAndCount(b)
-            count+=mergeAndCount(c)
-            i,j=0,0
-            # print('half of n is ', n//2)
-            for k in range(0,n):
-                # print(i,j)
-                if (i < n//2) & (j < n//2):
-                    if (b[i] < c[j]):
-                        a[k] = b[i]
-                        i+=1
-                    elif (b[i] > c[j]):
-                        a[k] = c[j]
-                        j+=1
-                        count+= len(b[i:])
-                else:
-                    if(i == n//2): a.extend(c[j:])
-                    elif(j == n//2): a.extend(b[i:])
-
-            print('Merged array: ', a)
-            return count
+                if(i == n//2): 
+                    d[k] = c[j]
+                    j+=1
+                elif(j == n//2):
+                    d[k] = b[i]
+                    i+=1
+                    
+        print('Merged array: ', d)
+        return d, x+y+z
 
 arr1 = [1,3,5,2,4,6]
 print(mergeAndCount(arr1))
